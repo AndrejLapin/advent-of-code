1ea80c0e6777c22ec52c862afdfaa31b2183b5cd
Andrej Lapin <lapin.andrej1@gmail.com>
2025-12-08 13:36:53 +0000

    2025 Day8-2

--- 2025/junction-boxes/junction-boxes.go
+++ 2025/junction-boxes/junction-boxes.go
@@ -33,7 +33,6 @@ func (j *JunctionBox) InitCircuit(circuit *Circuit) {
 
 func main() {
 	fileName := flag.String("input", "input.txt", "input file")
-	connectionCount := flag.Int("count", 1000, "connection count")
 	flag.Parse()
 
 	file, err := os.Open(*fileName)
@@ -44,7 +43,6 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	junctionBoxes := []JunctionBox{}
-	// circuits := []Circuit{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -67,7 +65,6 @@ func main() {
 
 	newPathIndex := 0
 	for outerBox := 0; outerBox < len(junctionBoxes)-1; outerBox++ {
-		// fmt.Println(junctionBox)
 		for innerBox := outerBox + 1; innerBox < len(junctionBoxes); innerBox++ {
 			xPow := math.Pow(float64(junctionBoxes[innerBox].X-junctionBoxes[outerBox].X), 2)
 			yPow := math.Pow(float64(junctionBoxes[innerBox].Y-junctionBoxes[outerBox].Y), 2)
@@ -84,21 +81,13 @@ func main() {
 		return connections[i].Distance < connections[j].Distance
 	})
 
-	// for index, _ := range connections {
-	// 	fmt.Printf("From: %+v; To: %+v; Distance - %d\n", *connections[index].PointA,
-	// 		*connections[index].PointB, connections[index].Distance)
-	// }
-
 	connectionIndex := 0
 	connectedAmount := 0
-	for connectedAmount < *connectionCount && connectionIndex < len(connections) {
+	for true {
 		// fmt.Printf("================== %d ===================\n", connectedAmount)
 		pointA := connections[connectionIndex].PointA
 		pointB := connections[connectionIndex].PointB
-		// fmt.Printf("Trying to connect %p and %p\n", pointA, pointB)
-		// fmt.Printf("A coords: %+v; B coords: %+v\n", pointA, pointB)
 		if pointA.ParentCircuit == pointB.ParentCircuit {
-			// fmt.Printf("Nodes already connected!\n")
 			connectedAmount++
 			connectionIndex++
 			continue
@@ -112,43 +101,24 @@ func main() {
 		}
 
 		if indexOfCurrentCircuit == -1 {
-			// fmt.Println("Something has gone terribly wrong with trying to find current circuit!")
 			panic("Something has gone terribly wrong with trying to find current circuit!")
 		}
 
-		// fmt.Printf("PointA parent circuit boxes - %v\n", pointA.ParentCircuit.JunctionBoxes)
-		// fmt.Printf("PointB parent circuit boxes - %v\n", pointB.ParentCircuit.JunctionBoxes)
-
 		pointB.ParentCircuit.JunctionBoxes = append(pointB.ParentCircuit.JunctionBoxes,
 			pointA.ParentCircuit.JunctionBoxes...)
-		// fmt.Printf("Junction boxes after appending - %v\n", pointB.ParentCircuit.JunctionBoxes)
+
+		if len(pointB.ParentCircuit.JunctionBoxes) == len(junctionBoxes) {
+			fmt.Printf("Point A: %+v; Point B: %+v\n", *pointA, *pointB)
+			fmt.Printf("X coords multiplied - %d\n", pointA.X*pointB.X)
+			break
+		}
 
 		for _, junctionBoxRef := range pointA.ParentCircuit.JunctionBoxes {
 			junctionBoxRef.ParentCircuit = pointB.ParentCircuit
-			// fmt.Printf("Assigning new parent to %p\n", junctionBoxRef)
-			// fmt.Printf("New parent boxes - %v\n", junctionBoxRef.ParentCircuit)
 		}
 
 		circuits[indexOfCurrentCircuit] = Circuit{}
 		connectedAmount++
 		connectionIndex++
 	}
-
-	sort.Slice(circuits, func(i, j int) bool {
-		return len(circuits[i].JunctionBoxes) > len(circuits[j].JunctionBoxes)
-	})
-
-	// for index, _ := range circuits {
-	// 	fmt.Println(circuits[index])
-	// }
-
-	result := 1
-
-	fmt.Printf("Biggest numbers: ")
-	for index := 0; index < 3; index++ {
-		number := len(circuits[index].JunctionBoxes)
-		fmt.Printf(" %d;", number)
-		result *= number
-	}
-	fmt.Printf("\nMultiplication result - %d\n", result)
 }
